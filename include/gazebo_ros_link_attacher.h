a60edb77ee82e2e916fdecc7a6b026d63685ab25
Peter <pmitrano@storm20.04>
2020-10-02 11:02:11 -0400

    clion auto reformat

--- include/gazebo_ros_link_attacher.h
+++ include/gazebo_ros_link_attacher.h
@@ -26,60 +26,62 @@
 namespace gazebo
 {
 
-   class GazeboRosLinkAttacher : public WorldPlugin
-   {
-      public:
-        /// \brief Constructor
-        GazeboRosLinkAttacher();
+class GazeboRosLinkAttacher : public WorldPlugin
+{
+ public:
+  /// \brief Constructor
+  GazeboRosLinkAttacher();
 
-        /// \brief Destructor
-        virtual ~GazeboRosLinkAttacher();
+  /// \brief Destructor
+  virtual ~GazeboRosLinkAttacher();
 
-        /// \brief Load the controller
-        void Load( physics::WorldPtr _world, sdf::ElementPtr /*_sdf*/ );
+  /// \brief Load the controller
+  void Load(physics::WorldPtr _world, sdf::ElementPtr /*_sdf*/);
 
-        /// \brief Attach with a revolute joint
-        bool attach(std::string model1, std::string link1,
-                    std::string model2, std::string link2);
+  /// \brief Attach with a revolute joint
+  bool attach(std::string model1, std::string link1,
+              std::string model2, std::string link2);
 
-        /// \brief Detach
-        bool detach(std::string model1, std::string link1,
-                    std::string model2, std::string link2);
+  /// \brief Detach
+  bool detach(std::string model1, std::string link1,
+              std::string model2, std::string link2);
 
-        /// \brief Internal representation of a fixed joint
-        struct fixedJoint{
-            std::string model1;
-            physics::ModelPtr m1;
-            std::string link1;
-            physics::LinkPtr l1;
-            std::string model2;
-            physics::ModelPtr m2;
-            std::string link2;
-            physics::LinkPtr l2;
-            physics::JointPtr joint;
-        };
+  /// \brief Internal representation of a fixed joint
+  struct fixedJoint
+  {
+    std::string model1;
+    physics::ModelPtr m1;
+    std::string link1;
+    physics::LinkPtr l1;
+    std::string model2;
+    physics::ModelPtr m2;
+    std::string link2;
+    physics::LinkPtr l2;
+    physics::JointPtr joint;
+  };
 
-        bool getJoint(std::string model1, std::string link1, std::string model2, std::string link2, fixedJoint &joint);
+  bool getJoint(std::string model1, std::string link1, std::string model2, std::string link2, fixedJoint &joint);
 
-   private:
-        ros::NodeHandle nh_;
-        ros::ServiceServer attach_service_;
-        ros::ServiceServer detach_service_;
+ private:
+  ros::NodeHandle nh_;
+  ros::ServiceServer attach_service_;
+  ros::ServiceServer detach_service_;
 
-        bool attach_callback(gazebo_ros_link_attacher::Attach::Request &req,
-                              gazebo_ros_link_attacher::Attach::Response &res);
-        bool detach_callback(gazebo_ros_link_attacher::Attach::Request &req,
-                             gazebo_ros_link_attacher::Attach::Response &res);
+  bool attach_callback(gazebo_ros_link_attacher::Attach::Request &req,
+                       gazebo_ros_link_attacher::Attach::Response &res);
 
-        std::vector<fixedJoint> joints;
+  bool detach_callback(gazebo_ros_link_attacher::Attach::Request &req,
+                       gazebo_ros_link_attacher::Attach::Response &res);
 
-        /// \brief The physics engine.
-        physics::PhysicsEnginePtr physics;
+  std::vector<fixedJoint> joints;
 
-        /// \brief Pointer to the world.
-        physics::WorldPtr world;
+  /// \brief The physics engine.
+  physics::PhysicsEnginePtr physics;
 
-   };
+  /// \brief Pointer to the world.
+  physics::WorldPtr world;
+
+};
 
 }
 
